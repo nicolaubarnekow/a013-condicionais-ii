add46599030cc3a3d97637d29270c0382cf9f983
nicolaubarnekow <mateustester@gmail.com>
2022-09-08 20:17:17 -0300

    aulinha12

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,15 @@
-const num = //seu numero aqui
+const num = Number(prompt('Descubra se o número é divisível por 2 ou por 3 ?'));
+
+   if(num % 2 ===0){
+    alert('Seu número é divisível por 2')
+    console.log('Seu número é divisível por 2')
+   } if(num % 3 ===0){
+    alert('O seu número é divisível por 3')
+    console.log('É divisível por 3')
+   } if(num % 2 === 0 && num % 3 === 0){
+    alert('Ele é divisível por 2 e por 3')
+   } else{
+    alert('Não é divisível por nenhum dos dois, tente novamente')
+    console.log('Não é divisível por nenhum dos dois, tente novamente')
+   }
 
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,23 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+let nacionalidade = prompt('digite aqui seu país de origem').toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+switch (nacionalidade) {
+  case 'brasil':
+    console.log('A pessoa é brasileira');
+    break;
+  case 'argentina':
+    console.log('A pessoa é argentina');
+    break;
+  case 'uruguai':
+    console.log('A pessoa uruguaia');
+    break;
+  case 'colombia':
+    console.log('A pessoa é colombiana');
+    break;
+  case 'chilena':
+    console.log('A pessoa é Chilena');
+    break;
+
+  default:
+    console.log('A pessoa é de NENHUM País');
+    break;
 }
